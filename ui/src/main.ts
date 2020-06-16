547484f1c7ee5470d9370e4a6a0fc57c42ccf501
sivohin_a <sivohin_a@protei.ru>
2020-06-16 14:59:26 +0300

    init

---
+++ ui/src/main.ts
@@ -0,0 +1,12 @@
+import Vue from "vue";
+import MainPage from "./views/MainPage.vue";
+import router from "./router";
+import store from "./store";
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: h => h(MainPage)
+}).$mount("#app");
---
+++ ui/src/views/MainPage.vue
@@ -0,0 +1,13 @@
+<template>
+    <div v-text="message">
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from "vue-property-decorator";
+
+    @Component
+    export default class MainPage extends Vue {
+        private message: string = 'Main Page';
+    }
+</script>
