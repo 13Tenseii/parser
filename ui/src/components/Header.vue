0cafa4b3dac24c3d70d25f314e0541c85ee2e7d0
sivohin_a <sivohin_a@protei.ru>
2020-06-18 19:46:22 +0300

    tmp

---
+++ ui/src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="Header">
+        <div class="Header__link"></div>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from "vue-property-decorator";
+
+    @Component
+    export default class Header extends Vue {
+    }
+</script>
--- ui/src/views/MainPage.vue
+++ ui/src/views/MainPage.vue
@@ -1,11 +1,12 @@
 <template>
-    <div v-text="message">
+    <div class="">
+        <img src="../../public/logo.png">
     </div>
 </template>
 
 <script lang="ts">
     import {Component, Vue} from "vue-property-decorator";
-
+    // font phantasm
     @Component
     export default class MainPage extends Vue {
         private message: string = 'Main Page';
